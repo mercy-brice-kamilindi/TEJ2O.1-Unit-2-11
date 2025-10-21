c53b815b404b7a00ab05b4afc966c39fd79e769d
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-10-21 13:29:05 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by:Mercy
+ * Created on:Oct 2025
+ * This program compars two numbers
 */
 
-basic.showString('Hello, World!')
+// variable
+let numberOne: number = 0
+let numberTwo: number = 0
+
+// clean
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+numberOne = randint(0,99)
+numberTwo = randint(0, 99)
+
+// show numberOne
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString('#1')
+    basic.showNumber(numberOne)
+    basic.clearScreen()
+})
+
+// show numberTwo
+input.onButtonPressed(Button.B, function() {
+    
+    basic.clearScreen()
+    basic.showString('#2')
+    basic.showNumber(numberTwo)
+    basic.clearScreen()
+    
+})
